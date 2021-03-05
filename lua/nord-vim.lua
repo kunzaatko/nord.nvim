8eeb8177238f7fd239586e0a24b5b2dc94021a06
Martin Kunz <martinkunz@email.cz>
2021-03-05 17:44:53 +0100

    Add: Comments with hex codes
    Adding comments with hexcodes after the colour definitions for porting.

--- lua/nord-vim.lua
+++ lua/nord-vim.lua
@@ -5,47 +5,47 @@ local v = vim.g -- this must be done, because lush is executed in a bare environ
 v.colors_name = 'nord'
 
 local clrs = {
-  nord0 = hsl(220, 16, 22),
-  nord1 = hsl(222, 16, 28),
-  nord2 = hsl(220, 17, 32),
-  nord3 = hsl(220, 16, 36),
-  nord3_bright = hsl(220, 17, 46),
-  nord4 = hsl(219, 28, 88),
-  nord5 = hsl(218, 27, 92),
-  nord6 = hsl(218, 27, 94),
-  nord7 = hsl(179, 25, 65),
-  nord8 = hsl(193, 43, 67),
-  nord9 = hsl(210, 34, 63),
-  nord10 = hsl(213, 32, 52),
-  nord11 = hsl(354, 42, 56),
-  nord12 = hsl(14, 51, 63),
-  nord13 = hsl(40, 71, 73),
-  nord14 = hsl(92, 28, 65),
-  nord15 = hsl(311, 20, 63),
+  nord0 = hsl(220, 16, 22), -- #2F3541
+  nord1 = hsl(222, 16, 28), -- #3C4353
+  nord2 = hsl(220, 17, 32), -- #444D5F
+  nord3 = hsl(220, 16, 36), -- #4D576A
+  nord3_bright = hsl(220, 17, 46), -- #616F89
+  nord4 = hsl(219, 28, 88), -- #D8DEE9
+  nord5 = hsl(218, 27, 92), -- #E5E9F0
+  nord6 = hsl(218, 27, 94), -- #ECEFF4
+  nord7 = hsl(179, 25, 65), -- #8FBCBB
+  nord8 = hsl(193, 43, 67), -- #87BFCF
+  nord9 = hsl(210, 34, 63), -- #81A1C1
+  nord10 = hsl(213, 32, 52), -- #5D81AC
+  nord11 = hsl(354, 42, 56), -- #BE6069
+  nord12 = hsl(14, 51, 63), -- #D18771
+  nord13 = hsl(40, 71, 73), -- #EBCA89
+  nord14 = hsl(92, 28, 65), -- #A4BF8D
+  nord15 = hsl(311, 20, 63), -- #B48EAD
 }
 
 local nord3_brightened = {
   clrs.nord3,
-  hsl(221, 17, 37),
-  hsl(219, 17, 38),
-  hsl(220, 17, 39),
-  hsl(220, 16, 40),
-  hsl(221, 16, 41),
-  hsl(221, 16, 42),
-  hsl(220, 17, 43),
-  hsl(221, 17, 44),
-  hsl(219, 17, 45),
-  hsl(220, 17, 46),
-  hsl(219, 17, 47),
-  hsl(221, 16, 48),
-  hsl(221, 16, 49),
-  hsl(220, 17, 50),
-  hsl(221, 16, 51),
-  hsl(220, 17, 52),
-  hsl(219, 17, 53),
-  hsl(220, 16, 54),
-  hsl(219, 16, 55),
-  hsl(219, 17, 56),
+  hsl(221, 17, 37), -- #4E586E
+  hsl(219, 17, 38), -- #505C71
+  hsl(220, 17, 39), -- #535E74
+  hsl(220, 16, 40), -- #566176
+  hsl(221, 16, 41), -- #586279
+  hsl(221, 16, 42), -- #5A657C
+  hsl(220, 17, 43), -- #5B6780
+  hsl(221, 17, 44), -- #5D6983
+  hsl(219, 17, 45), -- #5F6D86
+  hsl(220, 17, 46), -- #616F89
+  hsl(219, 17, 47), -- #63728C
+  hsl(221, 16, 48), -- #67738E
+  hsl(221, 16, 49), -- #697691
+  hsl(220, 17, 50), -- #6A7895
+  hsl(221, 16, 51), -- #6E7B96
+  hsl(220, 17, 52), -- #707E99
+  hsl(219, 17, 53), -- #73819C
+  hsl(220, 16, 54), -- #77839C
+  hsl(219, 16, 55), -- #7A879F
+  hsl(219, 17, 56), -- #7C89A2
 }
 
 local cfg = {
@@ -370,6 +370,7 @@ local theme = lush(function()
     lispFunc {Function},
 
     luaFunc {Function},
+    luaConstant {Boolean}, -- TODO: Could also be linked to Number for being violet <04-03-21, kunzaatko>
 
     perlPackageDecl {fg = clrs.nord7},
 
