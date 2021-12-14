2f5e78ceb9c7ec3539b02f0996be0062d5fdfc5d
Martin Kunz <martinkunz@email.cz>
2021-12-14 14:38:40 +0100

    fix(#8): add gui_combine for multiple guis
    [fixes: #8]

--- lua/pkgs/vimtex.lua
+++ lua/pkgs/vimtex.lua
@@ -4,6 +4,7 @@ local base = require 'base'
 
 local clrs = palette.clrs
 local spec = palette.spec
+local gui_combine = palette.gui_combine
 local pkg = function()
   return {
     texDelim {fg = clrs.nord15.da(10)},
@@ -17,18 +18,17 @@ local pkg = function()
 
     texEnvArgName {fg = clrs.nord13, gui = spec.bold},
     texEnvOpt {fg = clrs.nord7},
-    texPartArgTitle {fg = base.String.fg, gui = spec.bold .. ',' .. spec.italic},
+    texPartArgTitle { fg = base.String.fg, gui = gui_combine { spec.bold, spec.italic } },
     -- FIX: Why doesn't this work? <08-12-21, kunzaatko> --
     texArg {fg = clrs.nord7},
     texTitleArg {
       fg = texPartArgTitle.fg,
-      gui = spec.bold .. ',' .. spec.italic .. ',' .. spec.underline,
+      gui = gui_combine { spec.bold, spec.italic, spec.underline },
     },
     texRefArg {fg = texPartArgTitle.fg, gui = spec.italic},
     texFilesOpt {texEnvOpt},
 
     texMathSuperSub {fg = clrs.nord11},
-    texCmdGreek {fg = clrs.nord13, gui = spec.italic .. ',' .. spec.bold},
     texMathSymbol {fg = clrs.nord13},
     texSIArgNumU {base.String},
     texMathZoneW {fg = clrs.nord9},
@@ -37,6 +37,7 @@ local pkg = function()
     texMathOper {fg = clrs.nord11.da(10), gui = spec.bold},
     texMathSub {texMathSuperSub},
     texMathSuper {texMathSuperSub},
+    texCmdGreek { fg = clrs.nord13, gui = gui_combine { spec.italic, spec.bold } },
 
     texGroup {fg = clrs.nord7},
     texTheoremEnvOpt {texPartArgTitle},
